b306bd8c7d1cecd24119cdf6171da4d41ba4e041
Konrad Gorski <konradgrsk@gmail.com>
2020-10-07 14:09:55 +0200

    dbl click fir new entry form popup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
 import { listLogEntires } from './API';
 import LogEntryPopup from './components/LogEntryPopup';
+import LogEntryForm from './components/LogEntryForm';
 
 mapboxgl.accessToken =
   'pk.eyJ1Ijoia29udGVudHkiLCJhIjoiY2tmcGYweWtxMDRxMDJ3cnA1c2ZyaGYwaiJ9.d5CW9SPawU-ahL_1f7Lavg';
@@ -12,21 +13,46 @@ function App() {
   const [map, setMap] = useState(null);
   const mapContainerRef = useRef(null);
   // const markerRef = useRef(new mapboxgl.Marker());
-  // const popupRef = useRef(new mapboxgl.Popup({offset: 25}));
+  /* const formPopupRef = useRef(
+    new mapboxgl.Popup({
+      offset: 2,
+      className: 'form-popup',
+      maxWidth: '400px',
+    })
+  ); */
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
+    const newMap = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-73, 40],
-      zoom: 8,
+      zoom: 5,
+      doubleClickZoom: false,
     });
 
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-    map.on('load', () => {
-      map.resize();
-      console.log('set map');
-      setMap(map);
+    newMap.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    newMap.on('load', () => {
+      newMap.resize();
+      newMap.on('dblclick', (event) => {
+        const formPopup = new mapboxgl.Popup({
+          offset: 2,
+          className: 'form-popup',
+          maxWidth: '400px',
+        });
+        const removePopup = () => {
+          formPopup.remove();
+        };
+
+        const { lngLat } = event;
+        const popupNode = document.createElement('div');
+        ReactDOM.render(
+          <LogEntryForm lngLat={lngLat} removePopup={removePopup} />,
+          popupNode
+        );
+        formPopup.setLngLat(lngLat).setDOMContent(popupNode).addTo(newMap);
+      });
+
+      setMap(newMap);
     });
 
     (async () => {
@@ -35,7 +61,7 @@ function App() {
       setLogEntries(newLogEntries);
     })();
 
-    return () => map.remove();
+    return () => newMap.remove();
   }, []);
 
   useEffect(() => {
